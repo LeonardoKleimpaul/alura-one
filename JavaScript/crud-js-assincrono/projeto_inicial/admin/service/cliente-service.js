ade7c8076bf26f1222098de3b2df414df39198e6
LeonardoKleimpaul <leonardokleimpaul@protonmail.com>
2023-01-23 21:27:54 -0300

    finalizaçao Doguito Petshop

--- JavaScript/crud-js-assincrono/projeto_inicial/admin/service/cliente-service.js
+++ JavaScript/crud-js-assincrono/projeto_inicial/admin/service/cliente-service.js
@@ -1,7 +1,10 @@
 // Fetch
 const listaClientes = () => {
     return fetch('http://localhost:3000/profile').then((resposta) => {
-        return resposta.json();
+        if (resposta.ok) {
+            return resposta.json();
+        }
+        throw new Error('Não foi possível lista os clientes.');
     });
 };
 
@@ -16,19 +19,47 @@ const criaCliente = (nome, email) => {
             email: email,
         }),
     }).then((resposta) => {
-        return resposta.body;
+        if (resposta.ok) {
+            return resposta.body;
+        }
+        throw new Error('Não foi possível criar um cliente.');
     });
 };
 
 const removeCliente = (id) => {
     return fetch(`http://localhost:3000/profile/${id}`, {
         method: 'DELETE',
+    }).then((resposta) => {
+        if (!resposta.ok) {
+            throw new Error('Não foi possível remover um cliente.');
+        }
     });
 };
 
 const detalhaCliente = (id) => {
     return fetch(`http://localhost:3000/profile/${id}`).then((resposta) => {
-        return resposta.json();
+        if (resposta.ok) {
+            return resposta.json();
+        }
+        throw new Error('Não foi possível detalhar o cliente.');
+    });
+};
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email,
+        }),
+    }).then((resposta) => {
+        if (resposta.ok) {
+            return resposta.json();
+        }
+        throw new Error('Não foi possível atualizar o cliente.');
     });
 };
 
@@ -37,4 +68,5 @@ export const clienteService = {
     criaCliente,
     removeCliente,
     detalhaCliente,
+    atualizaCliente,
 };
